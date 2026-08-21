bc399afdbb580716430606f8a79fc548f38d4f9b
Harpreet <harpreet13108159@gmail.com>
2026-08-21 14:50:56 +0530

    added db config and added required models

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config.js";
-
+import prisma from "./src/config/database.js";
 
 const PORT=process.env.PORT || 5000;
 
@@ -10,6 +10,24 @@ app.use(cors())
 app.use(express.json());
 
 // app.use("/api",router)
+
+async function startServer() {
+  try {
+    await prisma.$connect();
+
+    console.log("Connected to PostgreSQL");
+
+    app.listen(PORT, () => {
+      console.log(`TaskFlow API running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed:", error);
+    process.exit(1);
+  }
+}
+
+
+startServer();
 
 
 app.listen(PORT,()=>{
